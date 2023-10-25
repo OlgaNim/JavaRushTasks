6221de46b7467028ea382976d5994fc53d69063d
OlgaNim <helganim@list.ru>
2023-10-25 21:47:21 +0300

    add solution javaSyntax task0206

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0206/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task02.task0206;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String word = "Солнечный";
+        String phrase = "Солнечный круг";
+        String line = "Солнечный круг, небо вокруг";
+        String text = "Солнечный круг, небо вокруг - это рисунок мальчишки";
+        //проверка инициализации
+        //System.out.println(word + phrase + line + text);
+
+    }
+}
