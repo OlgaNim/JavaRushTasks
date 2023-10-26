1720839caf7ba632c66264e7e892a111c477d05a
OlgaNim <helganim@list.ru>
2023-10-26 22:54:40 +0300

    add solution javaSyntax task0218

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0218/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task02.task0218;
+
+import java.util.Scanner;
+
+/* 
+Ручное управление
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number1 = sc.nextInt();
+        int number2 = sc.nextInt();
+        int number3 = sc.nextInt();
+        System.out.println(number1 * number2 * number3);
+    }
+}
