d9f316a5cfeaeb2a42bf701fde610ae845047c6e
OlgaNim <helganim@list.ru>
2023-10-26 23:03:28 +0300

    add solution javaSyntax task0215

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0215/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task02.task0215;
+
+import java.util.Scanner;
+
+/* 
+Чтение чисел
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number1 = sc.nextInt();
+        int number2 = sc.nextInt();
+        int number3 = sc.nextInt();
+        System.out.println((number1 + number2 + number3) / 3);
+    }
+}
