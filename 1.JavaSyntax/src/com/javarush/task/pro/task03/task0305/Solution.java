5df85c96cef171d03c3dd247e7d64ed5c13a3503
OlgaNim <helganim@list.ru>
2023-10-29 23:00:05 +0300

    add solution javaSyntax task0305

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0305/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.pro.task03.task0305;
+
+import java.util.Scanner;
+
+/* 
+Три числа
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number1 = sc.nextInt();
+        int number2 = sc.nextInt();
+        int number3 = sc.nextInt();
+        if (number1 == number2) {
+            System.out.print(number1 + " " + number2);
+            if (number2 == number3) {
+                System.out.println(" " + number3);
+            }
+        } else if (number2 == number3) {
+            System.out.println(number2 + " " + number3);
+        } else if (number1 == number3 ) {
+            System.out.println(number1 + " " + number3);
+        }
+    }
+}
