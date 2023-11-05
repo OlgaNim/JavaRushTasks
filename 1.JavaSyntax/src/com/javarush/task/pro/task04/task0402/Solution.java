7f7c010588937b6ca17ce65974de3d26a8ef7bf9
OlgaNim <helganim@list.ru>
2023-11-05 10:35:57 +0300

    add solution javaSyntax task0402

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0402/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task04.task0402;
+
+import java.util.Scanner;
+
+/* 
+Все любят Мамбу
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String text = " любит меня.";
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        int cycleStep = 0;
+        while (cycleStep < 10) {
+            System.out.println(name + text);
+            cycleStep++;
+        }
+    }
+}
