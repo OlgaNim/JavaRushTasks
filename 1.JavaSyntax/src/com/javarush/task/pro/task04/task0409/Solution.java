6482f1a746bff16b71c27662997736b84e4a0599
OlgaNim <helganim@list.ru>
2023-11-12 20:32:06 +0300

    add solution javaSyntax task0409 edit1

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0409/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0409/Solution.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int minimum = 2147483647;
+        int minimum = Integer.MAX_VALUE;
         int number;
         while (sc.hasNextInt()) {
             number = sc.nextInt();
