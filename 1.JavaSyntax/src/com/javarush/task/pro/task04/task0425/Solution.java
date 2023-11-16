4a4a7bbda78964d34afcbd88c54cd511b2c17b2f
OlgaNim <helganim@list.ru>
2023-11-16 20:43:38 +0300

    add solution javaSyntax task0425

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0425/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0425/Solution.java
@@ -14,7 +14,7 @@ public class Solution {
 
     public static void main(String[] args) {
 
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 10; j++) {
                 if (j == 3) {
                     System.out.print(hole);
@@ -25,21 +25,9 @@ public class Solution {
             System.out.println();
         }
 
-        for (int h = 0; h < 1; h++) {
-            for (int y = 0; y < 10; y++) {
-                if (y == 3) {
-                System.out.print(robot);
-                } else {
-                    System.out.print(land);
-                }
-            }
-
-        }
-        System.out.println();
-
         for (int k = 0; k < 1; k++) {
             for (int j = 0; j < 10; j++) {
-                System.out.print(bug);
+                System.out.print(death);
             }
         }
     }
