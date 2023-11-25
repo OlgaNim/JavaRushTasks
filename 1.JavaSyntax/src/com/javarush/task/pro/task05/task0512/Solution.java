65479a6a81a37d795fee1d1082ef750c290db09e
OlgaNim <helganim@list.ru>
2023-11-25 14:26:49 +0300

    add in process javaSyntax task0512

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0512/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.pro.task05.task0512;
+
+/* 
+Создаем мультимассив
+*/
+
+public class Solution {
+
+    public static int[][][] multiArray = new int[][][]
+            {{{4, 8, 15}, {16}},
+                    {{23, 42}, {}},
+                    {{1}, {2}, {3}, {4, 5}}};
+
+    public static void main(String[] args) {
+//        for (int i = 0; i < multiArray.length; i++) {
+//            for (int j = 0; j < multiArray[3].length; j++) {
+//                for (int k = 0; k < multiArray[3][4].length; k++) {
+//                    System.out.println(multiArray[i][j][k]);
+//        System.out.println(multiArray[0][0][0]);
+//        System.out.println(multiArray[0][0][1]);
+//        System.out.println(multiArray[0][0][2]);
+//        System.out.println(multiArray[0][1][0]);
+//
+//        System.out.println(multiArray[1][0][0]);
+//        System.out.println(multiArray[1][0][1]);
+//        System.out.println(multiArray[1][1][0]); //{}
+//
+//        System.out.println(multiArray[2][0][0]);
+//        System.out.println(multiArray[2][1][0]);
+//        System.out.println(multiArray[2][2][0]);
+//        System.out.println(multiArray[2][3][0]);
+//        System.out.println(multiArray[2][3][1]);
+
+
+
+                }
+                
+            }
+            
