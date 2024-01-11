49d71331d82910b053bba2b77052409de36b9117
OlgaNim <helganim@list.ru>
2024-01-11 10:37:47 +0300

    add solution javaSyntax task0606

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,52 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(String parameter){
+
+    }
+
+    public static void universalMethod(String parameter1, int number){
+
+    }
+
+    public static void universalMethod(int number1, int number2){
+
+    }
+
+    public static void universalMethod(boolean isExist){
+
+    }
+
+    public static void universalMethod(char a, String theme){
+
+    }
+
+    public static void universalMethod(double number1, int number2){
+
+    }
+
+    public static void universalMethod(float temperature){
+
+    }
+
+    public static void universalMethod(boolean isCat, boolean isHungry){
+
+    }
+
+    public static void universalMethod(String map, String weapon, String armor){
+
+    }
+
+    public static void universalMethod(int score, int time, boolean isReady){
+
+    }
+}
