ed45ddeae13664ee2ce4818d841255335b010030
OlgaNim <helganim@list.ru>
2024-01-24 00:13:19 +0300

    add solution javaSyntax task0701

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    int var1 = 109064887;
+    short var2 = 8376;
+    char var3 = 'a';
+    double var4 = 11.868005432;
+    float var5 = 34.15432f;
+    boolean isVar = true;
+    long var7 = -487908726;
+    byte var8 = 127;
+
+}
