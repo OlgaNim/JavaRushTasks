f8ae0cdf1a108ca87c08d3d36666803aba0a9182
OlgaNim <helganim@list.ru>
2024-01-24 00:37:49 +0300

    add solution javaSyntax task0702

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0702/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0702;
+
+/* 
+Максимальное целое
+*/
+
+public class Solution {
+    int MAX_VALUE = 2147483647;
+    short var2 = 32767;
+    char var3 = 65535;
+    double var4 = 1.7E+308;
+    float var5 = 3.4E+38f;
+    boolean isVar = true;
+    long var7 = 9223372036854775807L;
+    byte var8 = 127;
+}
