a823b523d9324b7b588065b50aeda34b97029567
OlgaNim <helganim@list.ru>
2024-01-28 00:46:24 +0300

    add solution javaSyntax task0719

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0719/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0719;
+
+/* 
+Космическая Робосутра
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        AutomaticKitchen kitchen = new AutomaticKitchen();
+        Gun gun = new Gun();
+        ProtectiveFieldGenerator generator = new ProtectiveFieldGenerator();
+        RepairDrone drone = new RepairDrone();
+        RobotCleaner robot = new RobotCleaner();
+    }
+}
