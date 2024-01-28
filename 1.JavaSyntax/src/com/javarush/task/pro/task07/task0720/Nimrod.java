bb5683faf01057084bc8363b69524b49cc2896a5
OlgaNim <helganim@list.ru>
2024-01-28 23:39:08 +0300

    add solution javaSyntax task0720

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0720/Nimrod.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task07.task0720;
+
+public class Nimrod {
+    public static int laser = 100000;
+    public static int rocket = 10000;
+    public static int angelicaKiss;
+    public int health = 1000000;
+
+    public void defend(int number) {
+        health = health - number;
+    }
+
+    public int attack() {
+        return rocket;
+    }
+
+}
+
